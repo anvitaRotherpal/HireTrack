68922776e50cee46a3bea6f42c1c70886d36b3e4
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-11-16 11:36:49 -0500

    first commit

---
+++ src/main/java/HireTrack/example/HireTrack/HireTrackApplication.java
@@ -0,0 +1,13 @@
+package HireTrack.example.HireTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HireTrack/example/HireTrack/HireTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package HireTrack.example.HireTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
