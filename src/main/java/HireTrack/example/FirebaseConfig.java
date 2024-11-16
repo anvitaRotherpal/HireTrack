970b65c0305aac93bb3bd2597275e091ed8b445a
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-11-16 13:09:40 -0500

    Completion of FirebaseConfig for proper integration of application with FireBase

--- src/main/java/HireTrack/example/FirebaseConfig.java
+++ src/main/java/HireTrack/example/FirebaseConfig.java
@@ -1,5 +1,25 @@
 package HireTrack.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
 public class FirebaseConfig {
-    
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("D:/HireTrack/src/main/resources/firebase-service-account.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
 }
