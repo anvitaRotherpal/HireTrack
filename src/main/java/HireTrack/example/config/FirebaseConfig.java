57d045965b4a58ca046c4c6b29805b33c6963089
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2026-01-06 15:44:07 -0500

    Added some changes

--- src/main/java/HireTrack/example/config/FirebaseConfig.java
+++ src/main/java/HireTrack/example/config/FirebaseConfig.java
@@ -3,29 +3,30 @@ package HireTrack.example.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream(
-                        getClass()
-                                .getClassLoader()
-                                .getResource("firebase-service-account.json")
-                                .getFile()
-                );
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/firebase-service-account.json");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        return FirebaseApp.initializeApp(options);
+            if(FirebaseApp.getApps().isEmpty()){
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
